3a250398cc8f23394c041eed9e9e94a198a7a882
Joe Malatesta <joemmalatesta@gmail.com>
2023-10-21 14:22:00 -0400

    add tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script lang='ts'>
-    
+
 </script>
 
 
-<h2>Upload Image</h2>
+<h2 class="text-5xl">Upload Image</h2>
 
 <form action="/process" method="POST" enctype="multipart/form-data">
   <input type="file" name="imageFile" accept="image/*">
