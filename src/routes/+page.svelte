2dc48de18c52bb7161582b7a6b675b1c91eb6672
Joe Malatesta <joemmalatesta@gmail.com>
2023-10-21 14:11:23 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang='ts'>
+    
+</script>
+
+
+<h2>Upload Image</h2>
+
+<form action="/process" method="POST" enctype="multipart/form-data">
+  <input type="file" name="imageFile" accept="image/*">
+  <button type="submit">Upload</button>
+</form>
